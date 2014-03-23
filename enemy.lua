49a421565b14daf3d91ae418fe6f6852786759f0
Stanislav Korotaev <superstals@gmail.com>
2014-03-23 23:00:29 +0400

    moving enemies

---
+++ enemy.lua
@@ -0,0 +1,29 @@
+require "player"
+
+function newEnemy(x, y)
+    local enemyInstance = {}
+
+	enemyInstance.x = x
+	enemyInstance.y = y
+
+	enemyInstance.width = 20
+	enemyInstance.height = 20
+	enemyInstance.speed = 125
+
+    return enemyInstance
+end
+
+function drawEnemy(self)
+	love.graphics.setColor(0, 0, 255)
+	love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
+
+function updateEnemy(self, dt)
+	vec = {x = player.x - self.x, y = player.y - self.y}
+	len = math.sqrt(vec.x * vec.x + vec.y * vec.y)
+	vec.x = vec.x / len
+	vec.y = vec.y / len
+
+	self.x = self.x + vec.x * self.speed * dt
+	self.y = self.y + vec.y * self.speed * dt
+end
--- main.lua
+++ main.lua
@@ -1,14 +1,39 @@
 require "player"
+require "enemy"
+
+enemies = {}
 
 function love.load()
 	player.load()
+
+	table.insert(enemies, newEnemy(100, 100))
+	table.insert(enemies, newEnemy(200, 200))
+	table.insert(enemies, newEnemy(300, 300))
 end
 
 function love.update(dt)
 	player.update(dt)
+	updateEnemies(dt)
 end
 
 function love.draw()
 	player.draw(dt)
+	drawEnemies()
 end
 
+
+function updateEnemies(dt)
+	len = table.getn(enemies)
+
+	 for i=1, len do
+		updateEnemy(enemies[i], dt)
+	end
+end
+
+function drawEnemies()
+	len = table.getn(enemies)
+
+	 for i=1, len do
+		drawEnemy(enemies[i])
+	end
+end
